094ec3758f366c49f60fbba16bc4eaff6ba3650e
agservizi <ag.servizi16@gmail.com>
2025-11-25 15:01:40 +0100

    Aumentato z-index del toast a 1100 per assicurarsi che appaia sopra le topbar

--- components/toast-stack.php
+++ components/toast-stack.php
@@ -1,1 +1,1 @@
-<div id="ap-toast-stack" class="toast-container position-fixed top-0 end-0 p-3" style="top: 120px; z-index: 11;" aria-live="polite" aria-atomic="true"></div>
+<div id="ap-toast-stack" class="toast-container position-fixed top-0 end-0 p-3" style="top: 120px; z-index: 1100;" aria-live="polite" aria-atomic="true"></div>
