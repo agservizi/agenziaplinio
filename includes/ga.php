26def75205fa1fa6f9d846ed923d36803cb202e8
agservizi <ag.servizi16@gmail.com>
2025-11-25 16:55:00 +0100

    Completamento al 100%: test PHPUnit funzionanti, commento per GA ID

--- includes/ga.php
+++ includes/ga.php
@@ -1,4 +1,5 @@
 <!-- Google Analytics 4 -->
+<!-- SOSTITUISCI GA_MEASUREMENT_ID con il tuo ID di misurazione Google Analytics 4 -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
